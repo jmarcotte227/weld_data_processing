f9dc1747479f600751a38de82891a9438f8027f3
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-01-31 17:46:35 -0500

    velocity debugging issues

--- streaming_tests/single_joint_exp/plot_joint_vel_procedural.py
+++ streaming_tests/single_joint_exp/plot_joint_vel_procedural.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data_dir='../../../recorded_data/streaming/'
+STREAMING_RATE = 125.
 
-for name in os.listdir(data_dir):
+for name in os.listdir(data_dir)[2:]:
     try:
         s=name.split('_')
         joint=int(s[2])
@@ -21,6 +22,12 @@ for name in os.listdir(data_dir):
         exe_time_diff = exe_stamps[1:]-exe_stamps[:-1]
         cmd_time_diff = cmd_stamps[1:]-cmd_stamps[:-1]
 
+        # artificial time difference from STREAMING_RATE
+        time_exe_stream = np.linspace(0, (len(cmd_stamps)-1)/STREAMING_RATE, len(cmd_stamps))
+        time_diff_stream = time_exe_stream[1:]-time_exe_stream[:-1] 
+        print(time_diff_stream[:5])
+        print(cmd_time_diff[:5])
+    
         # calculate joint difference
         exe_joint_diff = exe_joints[1:]-exe_joints[:-1]
         cmd_joint_diff = cmd_joints[1:]-cmd_joints[:-1]
@@ -28,6 +35,7 @@ for name in os.listdir(data_dir):
         # calculate velocity
         exe_vel = exe_joint_diff/exe_time_diff
         cmd_vel = cmd_joint_diff/cmd_time_diff
+        cmd_vel_adjust = cmd_joint_diff/time_diff_stream
 
 
         
@@ -36,10 +44,13 @@ for name in os.listdir(data_dir):
         fig,ax=plt.subplots()
         ax.scatter(exe_stamps[1:]-exe_stamps[0],exe_vel,s=1)
         ax.scatter(cmd_stamps[1:]-cmd_stamps[0],cmd_vel,s=1)
+        ax.scatter(time_exe_stream[1:],cmd_vel_adjust,s=1)
         ax.set_xlabel('time (s)')
         ax.set_ylabel('velocity (rad/s)')
         ax.set_title(f'Joint {joint+1} | Velocity {vel} rad/s')
         fig.savefig(f'procedural_plots/velocity_plots/joint_{joint+1}_vel_{vel}.png')
+        plt.show()
+        exit()
     except FileNotFoundError:
         pass
     except IndexError:
