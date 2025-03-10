a5919713e529f1f550d4f8937dc6aacbf5d4ef53
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-03-10 10:28:34 -0400

    presentation plots

--- insitu_correction/raw_error_plot.py
+++ insitu_correction/raw_error_plot.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-for layer in range (101,102):
+for layer in range (100,101):
     try:
         error_data = np.loadtxt(f'../../recorded_data/ER4043_bent_tube_large_hot_streaming_2025_03_06_feedback_troubleshooting/layer_{layer}/error.csv', delimiter=',')
-        error_data = error_data
-        time = np.linspace(0,len(error_data)/125, len(error_data))
+        error_data = error_data[200:-200]
+        time = np.linspace(0,len(error_data)/30, len(error_data))
         plt.plot(time,error_data)
         e_sum=0
         for e in error_data:
@@ -16,4 +16,5 @@ for layer in range (101,102):
         pass
 plt.xlabel("Time (s)")
 plt.ylabel("Error (mm)")
+plt.title(f"Layer {layer} Error")
 plt.show()
--- insitu_correction/vel_plot.py
+++ insitu_correction/vel_plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq
 
 
-for layer in range (101,102):
+for layer in range (104,105):
     try:
         cor_vel_data = np.loadtxt(f'../../recorded_data/ER4043_bent_tube_large_hot_streaming_2025_03_06_feedback_troubleshooting/layer_{layer}/v_plan.csv', delimiter=',')
         vel_data = np.loadtxt(f'../../recorded_data/ER4043_bent_tube_large_hot_streaming_2025_03_06_feedback_troubleshooting/layer_{layer}/v_cmd.csv', delimiter=',')
@@ -15,28 +15,30 @@ for layer in range (101,102):
         pass
 plt.xlabel("Time (s)")
 plt.ylabel("Velocity (mm/s)")
-plt.title("Layer 101 Velocity")
+plt.title(f"Layer {layer} Velocity")
 plt.legend()
 plt.show()
+# idx_start = 200
+# idx_end = -200
+# plt.plot(time_step[idx_start:idx_end], (cor_vel_data-vel_data)[idx_start:idx_end])
+# plt.xlabel("Time (s)")
+# plt.ylabel("Error (mm)")
+# plt.title(f"Layer {layer} Error")
+# plt.show()
 
-plt.plot(time_step[1000:-1000], (cor_vel_data-vel_data)[1000:-1000])
-plt.xlabel("Time (s)")
-plt.ylabel("Error (mm)")
-plt.show()
+# data = (cor_vel_data-vel_data)[1000:-1000]
+# data = data-np.mean(data)
+# t=time_step[1000:-1000]
 
-data = (cor_vel_data-vel_data)[1000:-1000]
-data = data-np.mean(data)
-t=time_step[1000:-1000]
+# N = len(data)
+# T = 1/125
 
-N = len(data)
-T = 1/125
-
-data_f=fft(data)
-tf = fftfreq(N,T)[:N//2]
-plt.plot(tf, 2.0/N * np.abs(data_f[0:N//2]))
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("Amplitude")
-plt.grid()
-plt.show()
+# data_f=fft(data)
+# tf = fftfreq(N,T)[:N//2]
+# plt.plot(tf, 2.0/N * np.abs(data_f[0:N//2]))
+# plt.xlabel("Frequency (Hz)")
+# plt.ylabel("Amplitude")
+# plt.grid()
+# plt.show()
 
 
