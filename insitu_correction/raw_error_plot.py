41de19f5764fdd311d3186adbcf7da38d82c7bc7
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-03-10 08:44:43 -0400

    making pres plots

--- insitu_correction/raw_error_plot.py
+++ insitu_correction/raw_error_plot.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-for layer in range (70,105):
+for layer in range (101,102):
     try:
         error_data = np.loadtxt(f'../../recorded_data/ER4043_bent_tube_large_hot_streaming_2025_03_06_feedback_troubleshooting/layer_{layer}/error.csv', delimiter=',')
-        error_data = error_data[200:-200]
-        plt.plot(error_data)
+        error_data = error_data
+        time = np.linspace(0,len(error_data)/125, len(error_data))
+        plt.plot(time,error_data)
         e_sum=0
         for e in error_data:
             e_sum+=e**2
@@ -13,4 +14,6 @@ for layer in range (70,105):
         print(f"Layer {layer} RMSE: ", rmse)
     except:
         pass
+plt.xlabel("Time (s)")
+plt.ylabel("Error (mm)")
 plt.show()
--- insitu_correction/vel_plot.py
+++ insitu_correction/vel_plot.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fft import fft, fftfreq
 
 
-for layer in range (71,72):
+for layer in range (101,102):
     try:
         cor_vel_data = np.loadtxt(f'../../recorded_data/ER4043_bent_tube_large_hot_streaming_2025_03_06_feedback_troubleshooting/layer_{layer}/v_plan.csv', delimiter=',')
         vel_data = np.loadtxt(f'../../recorded_data/ER4043_bent_tube_large_hot_streaming_2025_03_06_feedback_troubleshooting/layer_{layer}/v_cmd.csv', delimiter=',')
@@ -14,6 +15,28 @@ for layer in range (71,72):
         pass
 plt.xlabel("Time (s)")
 plt.ylabel("Velocity (mm/s)")
-plt.title("Layer 91 Velocity")
+plt.title("Layer 101 Velocity")
 plt.legend()
 plt.show()
+
+plt.plot(time_step[1000:-1000], (cor_vel_data-vel_data)[1000:-1000])
+plt.xlabel("Time (s)")
+plt.ylabel("Error (mm)")
+plt.show()
+
+data = (cor_vel_data-vel_data)[1000:-1000]
+data = data-np.mean(data)
+t=time_step[1000:-1000]
+
+N = len(data)
+T = 1/125
+
+data_f=fft(data)
+tf = fftfreq(N,T)[:N//2]
+plt.plot(tf, 2.0/N * np.abs(data_f[0:N//2]))
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Amplitude")
+plt.grid()
+plt.show()
+
+
