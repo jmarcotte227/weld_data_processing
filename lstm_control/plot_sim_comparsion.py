44bf8a2e35b7287bd1fdba792cdc3d0661c8e231
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-11-13 22:37:31 -0500

    plotting comparison changes

--- lstm_control/plot_sim_comparsion.py
+++ lstm_control/plot_sim_comparsion.py
@@ -26,13 +26,10 @@ errors_2 = np.loadtxt(
 # rms error calc
 fig, ax = plt.subplots(1,1)
 # sim measurement noise
-sim_meas = torch.load("sim_data/test_results_meas.pt")
+sim_meas = torch.load("sim_data/test_results_neg.pt")
 rms_meas = sim_meas["layer_error_all"]
+print(rms_meas)
 ax.plot(rms_meas)
-
-sim_proc = torch.load("sim_data/test_results_proc.pt")
-rms_proc = sim_proc["layer_error_all"]
-ax.plot(rms_proc)
 
 rms_errors = []
 for layer in range(errors_2.shape[0]):
@@ -44,7 +41,6 @@ ax.legend([
     # r"$\alpha=1.0, \beta=0.2$",
     # r"$\alpha=1.4, \beta=0.1$",
     'Simulation: Measurement Noise',
-    'Simulation: Process Noise',
     'Experimental',
     ''
 ])
