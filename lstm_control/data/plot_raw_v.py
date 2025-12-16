f2bc93e4e609fc2f2baea8188bb58ac7bd98b999
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2025-12-15 23:03:12 -0500

    new data

--- lstm_control/data/plot_raw_v.py
+++ lstm_control/data/plot_raw_v.py
@@ -5,9 +5,11 @@ from motoman_def import robot_obj, positioner_obj
 if __name__=='__main__':
     # DATASET = "2025_11_19_11_50_06_AL_WLJ_dataset0"
     # DATASET = 'wall_lstm_control_2025_11_05_13_17_59'
-    DATASET = "2025_12_03_11_29_23_WLJAL_velocity_testing2"
+    # DATASET = "2025_12_03_11_29_23_WLJAL_velocity_testing2"
+    DATASET =  "2025_12_12_14_46_12_WLJ_AL_new_delay_test"
+    # DATASET =  "2025_12_12_14_54_31_WLJ_AL_old_delay"
     REC_DIR = '../../../recorded_data/'
-    LAYER = 23
+    LAYER = 5
     # problem layers 2,4,8
 
     # load robot config
@@ -37,7 +39,7 @@ if __name__=='__main__':
     # DATASET = 'wall_lstm_control_2025_11_05_13_17_59'
     joint_data = np.loadtxt(f"{REC_DIR}{DATASET}/layer_{LAYER}/weld_js_exe.csv", delimiter=',')
     joint_data_cmd = np.loadtxt(f"{REC_DIR}{DATASET}/layer_{LAYER}/weld_js_cmd.csv", delimiter=',')
-    joint_data_ext = np.loadtxt(f"{REC_DIR}{DATASET}/layer_{LAYER}/meas_js_ext.csv", delimiter=',')
+    # joint_data_ext = np.loadtxt(f"{REC_DIR}{DATASET}/layer_{LAYER}/meas_js_ext.csv", delimiter=',')
     v_cmd = np.loadtxt(f"{REC_DIR}{DATASET}/layer_{LAYER}/v_cmd.csv", delimiter=',')
 
     t_start = joint_data[0,0]
@@ -50,7 +52,7 @@ if __name__=='__main__':
     time_difs=[]
     time_difs_cmd=[]
     time_difs_ext=[]
-    loop_times = np.loadtxt(f"{REC_DIR}{DATASET}/layer_{LAYER}/func_times.csv", delimiter=',')
+    # loop_times = np.loadtxt(f"{REC_DIR}{DATASET}/layer_{LAYER}/func_times.csv", delimiter=',')
     idx_low = 0
     idx_high = joint_data.shape[0]
     # for idx in range(joint_data.shape[0]):
@@ -96,47 +98,50 @@ if __name__=='__main__':
             time_stamps_cmd.append(time_stamp)
             time_difs_cmd.append(time_dif)
     idx_low = 0
-    idx_high = joint_data_ext.shape[0]
-    for idx in range(idx_low, idx_high):
-        robot1_pose=robot.fwd(joint_data_ext[idx][2:8])
-        time_stamp=joint_data_ext[idx][0]
-        # if idx==0:
-        if idx==idx_low:
-            pose_prev=robot1_pose.p
-            time_prev=time_stamp
-        else:
-            cart_dif=robot1_pose.p-pose_prev
-            time_dif = time_stamp-time_prev
-            time_prev = time_stamp
-            cart_vel = cart_dif/time_dif
-            time_prev = time_stamp
-            pose_prev = robot1_pose.p
-            lin_vel = np.sqrt(cart_vel[0]**2+cart_vel[1]**2)
-            # lin_vel = np.sqrt(cart_vel[0]**2+cart_vel[1]**2+cart_vel[2]**2)
-            cart_vels_ext.append(lin_vel)
-            time_stamps_ext.append(time_stamp)
-            time_difs_ext.append(time_dif)
+    # idx_high = joint_data_ext.shape[0]
+    # for idx in range(idx_low, idx_high):
+    #     robot1_pose=robot.fwd(joint_data_ext[idx][2:8])
+    #     time_stamp=joint_data_ext[idx][0]
+    #     # if idx==0:
+    #     if idx==idx_low:
+    #         pose_prev=robot1_pose.p
+    #         time_prev=time_stamp
+    #     else:
+    #         cart_dif=robot1_pose.p-pose_prev
+    #         time_dif = time_stamp-time_prev
+    #         time_prev = time_stamp
+    #         cart_vel = cart_dif/time_dif
+    #         time_prev = time_stamp
+    #         pose_prev = robot1_pose.p
+    #         lin_vel = np.sqrt(cart_vel[0]**2+cart_vel[1]**2)
+    #         # lin_vel = np.sqrt(cart_vel[0]**2+cart_vel[1]**2+cart_vel[2]**2)
+    #         cart_vels_ext.append(lin_vel)
+    #         time_stamps_ext.append(time_stamp)
+    #         time_difs_ext.append(time_dif)
 
     plot_min = 0
     plot_max = -1
     
     print(len(time_stamps_cmd))
-    print(len(loop_times))
-    fig, ax = plt.subplots(3,1, sharex=True)
-    ax[0].scatter(time_stamps-t_start, cart_vels)
-    ax[0].scatter(time_stamps_cmd-t_start, cart_vels_cmd)
-    ax[0].scatter(time_stamps_ext-t_start, cart_vels_ext)
+    fig, ax = plt.subplots(2,1, sharex=True)
+    # fig.set_size_inches(10, 5)
+    # ax[0].scatter(time_stamps-t_start, cart_vels)
+    # ax[0].scatter(time_stamps_cmd-t_start, cart_vels_cmd)
+    ax[0].scatter(time_stamps_cmd-t_start, joint_data_cmd[:-1,5])
+
+    # ax[0].scatter(time_stamps_ext-t_start, cart_vels_ext)
     # ax[0].scatter(time_stamps_cmd-t_start, v_cmd[1:])
     ax[1].scatter(time_stamps-t_start, time_difs)
     ax[1].scatter(time_stamps_cmd-t_start, time_difs_cmd)
     ax[1].scatter(time_stamps_ext-t_start, time_difs_ext)
+    ax[1].plot([time_stamps_cmd[0]-t_start, time_stamps_cmd[-1]-t_start], [0.008, 0.008], 'r--')
     ax[0].set_title("Velocitys")
-    ax[0].set_ylabel("Velocity (mm/s)")
+    # ax[0].set_ylabel("Velocity (mm/s)")
+    ax[0].set_ylabel("Joint 4 Position (rad)")
     ax[1].set_title("Time Step")
     ax[1].set_xlabel("Clock Time (s)")
     ax[1].set_ylabel("Time Step (s)")
-    ax[0].legend(["Measured","Commanded Joint", "Measure Ext"])
-    ax[2].scatter(time_stamps_cmd-t_start, loop_times[:-1])
+    ax[0].legend(["Measured","Commanded Joint"])
     plt.show()
 
 
